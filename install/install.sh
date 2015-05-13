d519d2d8a93d361a9f000f40164105c887a8cc56
MartinArroyo <martinarroyo@usal.es>
2015-05-13 15:55:45 +0200

    Successful test

---
+++ install/install.sh
@@ -0,0 +1,14 @@
+pacman -S openldap nss-pam-ldapd
+
+echo "Performing some tests. Please enter username to try to authenticate with the server"
+read username
+
+ldapsearch -x -D uid=$username,ou=people,dc=DIA -W -H ldap://ldap1.cie.aulas.usal.es:389 -b dc=dia -s sub uid=$username
+
+echo "Starting nslcd.service"
+
+systemctl start nslcd.service
+
+echo "Testing connection"
+
+getent passwd
