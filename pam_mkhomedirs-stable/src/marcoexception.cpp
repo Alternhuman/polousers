84d8036fc7d0f148419e4a0313c5416a54d74840
MartinArroyo <martinarroyo@usal.es>
2015-05-27 18:34:22 +0200

    Deleted old files

--- pam_mkhomedirs-stable/src/marcoexception.cpp
+++
@@ -1,8 +0,0 @@
-#include "marcoexception.h"
-#include <stdio.h>
-using namespace std;
-
-
-marcoexception::marcoexception(char const* message){
-	snprintf(thrownmessage, sizeof thrownmessage, message);
-}
